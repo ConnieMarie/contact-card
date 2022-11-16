9e834452061a1bb38fe2397a306d93d1c1ec1a72
ConnieMarie <constance.marie.strahan@gmail.com>
2022-11-16 11:48:25 -0600

    configure/connect indexeddb

---
+++ client/src/js/index.js
@@ -0,0 +1,22 @@
+// import modules
+import  "./form";
+import "./submit";
+// import css files
+import "../css/index.css";
+import { Tooltip, Toast, Popover} from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import image files
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+// import db files
+import { initdb } from './database';
+
+
+window.addEventListener('load', function () {
+    initdb();
+    document.getElementById('logo').src = Logo;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+    
+  });
